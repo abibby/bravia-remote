64f6ea783418f2b8aef2e55b0c573ae10685489e
Adam Bibby <abibby@mero.co>
2023-10-18 12:49:08 -0400

    add bravia

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/abibby/fileserver v0.0.0-20221208191705-087686654550
+	github.com/davecgh/go-spew v1.1.1
 	github.com/gorilla/websocket v1.5.0
 	github.com/joho/godotenv v1.5.1
 	github.com/pkg/errors v0.9.1
--- ui/src/bravia/avContent.ts
+++ ui/src/bravia/avContent.ts
@@ -8,19 +8,16 @@ export interface Source {
     source: string
 }
 
-export const getSourceList = braviaAPIFactory<[options:GetSourceListOptions], Source[]>(
-    'avContent',
-    'getSourceList',
-    '1.0',
-)
+export const getSourceList = braviaAPIFactory<
+    [options: GetSourceListOptions],
+    Source[]
+>('avContent', 'getSourceList', '1.0')
 
 export interface SetPlayContentOptions {
     uri: string
 }
 
-
-export const setPlayContent = braviaAPIFactory<[options:SetPlayContentOptions], void>(
-    'avContent',
-    'setPlayContent',
-    '1.0',
-)
+export const setPlayContent = braviaAPIFactory<
+    [options: SetPlayContentOptions],
+    void
+>('avContent', 'setPlayContent', '1.0')
